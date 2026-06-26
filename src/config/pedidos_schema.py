70ce2539bac0925d897f69209c7fc1036131c7eb
Abethiol <lucasbethiol@gmail.com>
2026-06-26 19:28:03 -0300

    correcoes nos schemas e settings

--- src/config/pedidos_schema.py
+++ src/config/pedidos_schema.py
@@ -3,14 +3,14 @@ from pyspark.sql.types import (
     StructField,
     StringType,
     DoubleType,
-    IntegerType
+    LongType
 )
 
 PEDIDOS_SCHEMA = StructType([
     StructField("ID_PEDIDO", StringType(), True),
     StructField("PRODUTO", StringType(), True),
     StructField("VALOR_UNITARIO", DoubleType(), True),
-    StructField("QUANTIDADE", IntegerType(), True),
+    StructField("QUANTIDADE", LongType(), True),
     StructField("DATA_CRIACAO", StringType(), True),
     StructField("UF", StringType(), True),
     StructField("ID_CLIENTE", StringType(), True)
